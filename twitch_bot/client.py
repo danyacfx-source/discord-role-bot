257cbfdb4caa1ccb7c34f15868b2f9306761ac10
danyacfx-source <danyacfx-source@users.noreply.github.com>
2026-08-21 09:56:57 +0300

    Twitch: промо Discord ссылки в чат каждые 10 минут

--- twitch_bot/client.py
+++ twitch_bot/client.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import twitchio
@@ -22,6 +23,9 @@ class TwitchChatClient(twitchio.Client):
         self.chat_commands = CommandHandler(config, self)
         self.moderation = Moderation(config.get("moderation") or {})
         self.greetings = Greetings(config.get("greetings") or {})
+        self._promo_task = None
+        self._promo_message = config.get("promo_message", "")
+        self._promo_interval = config.get("promo_interval_seconds", 600)
 
     async def event_ready(self):
         self.channel = self.get_channel(self.cfg["channel"])
@@ -29,6 +33,21 @@ class TwitchChatClient(twitchio.Client):
         log.info("Twitch: подключён как %s (канал #%s)", self.nick, self.cfg["channel"])
         if self.bridge is not None:
             self.bridge.channel_id = self.cfg.get("discord_channel_id", 0)
+        if self._promo_message and self._promo_interval > 0:
+            self._promo_task = self.loop.create_task(self._promo_loop())
+
+    async def _promo_loop(self):
+        await asyncio.sleep(self._promo_interval)
+        while True:
+            try:
+                if self.channel and self._promo_message:
+                    await self.channel.send(self._promo_message)
+                    log.info("Twitch: промо-сообщение отправлено")
+            except asyncio.CancelledError:
+                break
+            except Exception:
+                log.exception("Twitch: ошибка отправки промо")
+            await asyncio.sleep(self._promo_interval)
 
     async def event_message(self, message):
         if message.echo:
